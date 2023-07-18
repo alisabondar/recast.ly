f5edac03384a5943fa6dd7c5c5ebb82c01b1ec62
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-07-17 20:38:06 -0400

    rendered videos

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,14 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
+
+const VideoList = ({exampleVideoData}) => {
+  return (
+    <div className="video-list">
+      <div><h5>{exampleVideoData.map((video) => {
+        return <VideoListEntry video = {video} />;
+      })}</h5></div>
+    </div>
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // TODO: Render the `App` component to the DOM
+// import React from 'react';
+// import ReactDOM from 'ReactDOM';
 import App from './components/App.js';
 
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
